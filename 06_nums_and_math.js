2d37d8dbe041a9606e41f0ffd7b628645a213169
Rajesh74596 <rajeshrk74596@gmail.com>
2026-01-24 19:45:30 +0530

    change in config

--- 06_nums_and_math.js
+++ 06_nums_and_math.js
@@ -1,4 +1,4 @@
-const score = 400;
+// const score = 400;
 // console.log(score);
 
 const balance = new Number(100);
@@ -17,11 +17,20 @@ const hundreds = 1000000;
 // +++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++
 
 // console.log(Math);
-// console.log(Math.abs(-5));
-// console.log(Math.round(5.2));
-// console.log(Math.ceil(5.2));
-// console.log(Math.floor(5.2));
-// console.log(Math.sqrt(5));
-// console.log(Math.min(5, 3, 4, 6));
-// console.log(Math.max(5, 3, 4, 6));
+
+// console.log(Math);
+// console.log(Math.abs(-7));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(5.8));
+// console.log(Math.floor(5.8));
+// console.log(Math.max(5, 8, 6, 4, 7));
+// console.log(Math.min(5, 8, 6, 4, 7));
+
 console.log(Math.random());
+console.log(Math.random() * 10 + 1);
+console.log(Math.random(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
