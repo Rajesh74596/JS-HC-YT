623d95495e74fd0bf0f60bfb94b9c6c0b0cd7870
Rajesh74596 <rajeshrk74596@gmail.com>
2026-01-27 15:04:10 +0530

    changes in config

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,33 @@
+// Singleteon
+// Objesct iterals
+
+const mySym = Symbol["key1"];
+
+const JsUser = {
+  name: "rajesh",
+  [mySym]: "myKey1",
+  age: 27,
+  location: "hyderabad",
+  email: "rajesh@google.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "saturday"],
+};
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "rajesh@chatgppty.com";
+// Object.freeze(JsUser);
+JsUser.email = "rajesh@hnaili.com";
+// console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("hello Js User");
+};
+
+JsUser.greetingTwo = function () {
+  console.log(`hello Js User, ${this.name}`);
+};
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
