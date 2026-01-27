26ede85910846abf98ab79a11c112af57a71f9a4
Rajesh74596 <rajeshrk74596@gmail.com>
2026-01-27 13:39:03 +0530

    changes in config

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,10 +1,10 @@
 //Array
 
-const myArr = [0, 1, 2, 3];
+const myArr = [0, 1, 2, 3, 4, 5];
 
 const myHeroes = ["shaktiman", "Ironman", "heman"];
 
-const myArr2 = new Array(1, 2, 3, 4);
+const myArr2 = new Array(1, 2, 3, 4, 5, 6);
 
 //console.log(MyArr[0]);
 
@@ -29,7 +29,8 @@ console.log("A", myArr);
 const myn1 = myArr.slice(1, 3);
 
 console.log(myn1);
-console.log("B", myArr);
 
-const myn2 = myArr.slice(1, 3);
-console.log(my);
+console.log("B", myArr2);
+
+const myn2 = myArr2.splice(1, 3);
+console.log(myArr2);
