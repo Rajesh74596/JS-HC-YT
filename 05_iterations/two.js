5d167623f84b1c55c2cb71645a438af37de19d28
Rajesh74596 <rajeshrk74596@gmail.com>
2026-03-07 22:35:39 +0530

    changes in config

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// let index = 0;
+
+// while (index <= 10) {
+//   console.log(`value of index is ${index}`);
+//   index = index + 2;
+// }
+
+let myArrray = ["flashman", "superman", "batman"];
+
+let arr = 0;
+
+while (arr < myArrray.length) {
+  console.log(`value is ${myArrray[arr]}`);
+  arr = arr + 1;
+}
+
+let score = 10;
+
+do {
+  console.log(`score is ${score}`);
+  score++;
+} while (score >= 7);
