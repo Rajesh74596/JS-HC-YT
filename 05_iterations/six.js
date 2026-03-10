72e5f22a2653c8719f5b5ea2b6eb08df96d5ba1c
Rajesh74596 <rajeshrk74596@gmail.com>
2026-03-10 11:48:50 +0530

    changes in config

---
+++ 05_iterations/six.js
@@ -0,0 +1,33 @@
+// const coding = ["js", "ruby", "java", "cpp"];
+
+// const values = coding.forEach((item) => {
+//   console.log(item);
+//   return item;
+// });
+
+// console.log(values);
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const newnums = myNums.filter((num) => {
+//   return num > 4;
+// });
+
+// console.log(newnums);
+
+// const newNum = [];
+
+// myNums.forEach((num) => {
+//   if (num > 4) {
+//     newNum.push(num);
+//   }
+// });
+// console.log(newNum);
+
+// let userBooks = books.filter((bk) => bk.genre === "history");
+// // console.log(userBooks);
+
+// userBooks = books.filter((bk) => {
+//   return bk.genre === "history" && bk.genre === "1995";
+// });
+// console.log(userBooks);
